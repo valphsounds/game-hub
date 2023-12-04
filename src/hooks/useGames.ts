dbe44079af57b8f9bfaf1f5d41a571cccd138482
Elias Lang <hello@eliaslang.com>
2023-12-04 16:33:41 +0100

    Filter games by platform.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,22 +1,32 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[]
-    metacritic: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: { genres: selectedGenre?.id}}, [selectedGenre?.id]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
-export default useGames
+export default useGames;
